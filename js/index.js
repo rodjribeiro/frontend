3b7a8df4cb0a474cb0722a2839a22a1ed763adf0
henriqcs <casareshenrique@gmail.com>
2020-05-14 21:28:15 -0300

    splitting html, css and js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var form = document.getElementById('form')
+form.addEventListener('submit', function(e) {
+  
+//auto submission of the form
+e.preventDefault()
+
+var name = document.getElementById('name').value
+var empresa = document.getElementById('empresa').value
+var gastos = document.getElementById('gastos').value
+//var ano = document.getElementById('ano').value
+//var price = document.getElementById('price').value
+//var email = document.getElementById('email').value
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+	.then(response => response.json())
+	.then(json => console.log(json))
